5d249b4ef523ba943eee87ca6ae5f19b82d6ef2d
frsantens <frsantens@gmail.com>
2025-06-28 12:02:00 +0200

    main.py accepts filepath as argument

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from stats import get_num_words, get_char_dict, dict_sorted_list
+import sys
+
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     num_words = get_num_words(book_text)
     char_dict = get_char_dict(book_text)
     char_list = dict_sorted_list(char_dict)
-
     return print_report(filepath, num_words, char_list)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+
 
 def print_report(filepath, num_words, char_list):
     print("============ BOOKBOT ============")
